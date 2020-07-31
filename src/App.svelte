8999c3f3924dca4b97dce10abacdd8c9bf66d839
fabezio <fabezio@outlook.fr>
2020-07-31 14:58:03 +0200

    import routify, ajout index

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
-	export let name;
+	import { routify } from '@sveltech/routify'
+	// export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<!-- <h1>Hello {name}!</h1> -->
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
